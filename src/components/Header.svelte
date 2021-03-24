63aa9e4c097f6d43525cada66ac6e89ff8321641
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-03-24 21:41:09 +0100

    init coronaampel styles

---
+++ src/components/Header.svelte
@@ -0,0 +1,29 @@
+<style>
+    .header {
+      display: block;
+      grid-area: header;
+      position: sticky;
+      top: 0;
+      z-index: 10;
+    }
+  
+    header {
+      background-color: var(--bg-200);
+      box-shadow: var(--box-shadow);
+      display: block;
+      position: relative;
+      text-align: center;
+      z-index: 10;
+    } 
+
+  </style>
+  
+  <script>
+
+  </script>
+  
+  <div class="header">
+    <header>
+      LOGO
+    </header>
+  </div>
